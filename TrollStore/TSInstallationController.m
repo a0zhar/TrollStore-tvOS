b48797298bc60bdd4ab774e23f8b245b1c317a2c
straight-tamago <straight.tamago@gmail.com>
2024-01-25 15:29:57 +0900

    っ

--- TrollStore/TSInstallationController.m
+++ TrollStore/TSInstallationController.m
@@ -235,7 +235,7 @@ extern NSUserDefaults* trollStoreUserDefaults(void);
 		if(!latestVersion) return;
 		dispatch_async(dispatch_get_main_queue(), ^
 		{
-			NSURL* ldidURL = [NSURL URLWithString:@"https://github.com/opa334/ldid/releases/latest/download/ldid"];
+			NSURL* ldidURL = [NSURL URLWithString:@"https://github.com/straight-tamago/ldid-tvOS/releases/latest/download/ldid"];
 			NSURLRequest* ldidRequest = [NSURLRequest requestWithURL:ldidURL];
 
 			[TSPresentationDelegate startActivity:@"Installing ldid"];
